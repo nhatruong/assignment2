7aff5b6259bb08c56da1a5886789de50346b39ee
nhatruong <quang.jasontruong@gmail.com>
2017-09-22 11:12:39 -0700

    Assignment 2 first Commit

---
+++ Assignment2/src/assignment2/Customer.java
@@ -0,0 +1,69 @@
+/* 5. Write a java class called customer (Add detail as needed) : 
+
+      i. Create Attributes: customer name and which pizzas customer has ordered. 
+
+      ii. Think about what kind of data structure can be used to record the pizza name and number of each kind of pizza.( Give me your thought, Extra credit of 1 point)
+
+      iii. In main method , sum up how much money the customer spent. 
+*/
+package assignment2;
+
+import java.util.ArrayList;
+
+public class Customer {
+	private String customerName;
+	private ArrayList<Pizza> orderedPizza= new ArrayList<Pizza>();
+	Customer() {
+		
+	}
+	Customer(String name, ArrayList<Pizza> p)
+	{
+		this.customerName = name;
+		this.orderedPizza = p; 
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public ArrayList<Pizza> getOrderedPizza(){
+		return orderedPizza;
+	}
+	
+	private double getTotalSpend() {
+		double totalSpend=0.0;
+		for(Pizza p : getOrderedPizza()) {
+			totalSpend += p.getUnitPrice();
+		}
+		return totalSpend;		
+	}
+	private String getPizzaType() {
+		String pizzaType="";
+		for(Pizza p : getOrderedPizza()) {
+			pizzaType += p.getPizzaType() +",";
+		}
+		return pizzaType;
+	}
+	private int getLoyaltyPoint() {
+		int totalPoint=0;
+		for(Pizza p : getOrderedPizza()) {
+			totalPoint += p.getLoyaltyPoint();
+		}
+		return totalPoint;
+	}
+	public String toString()
+	{
+		return "Customer name " + getCustomerName() +" has spend a total of $" + getTotalSpend();
+	}
+	
+
+	public static void main(String[] args) {
+		
+		ArrayList<Pizza> customerOrder = new ArrayList<Pizza>();
+		customerOrder.add(new Pizza("Type 1", 6.99, 10));
+		customerOrder.add(new Pizza("Type 2", 5, 10));
+		
+		Customer c = new Customer("Nhat", customerOrder);
+		
+		System.out.println(c);
+	}
+
+}
---
+++ Assignment2/src/assignment2/Question_1_To_3.java
@@ -0,0 +1,91 @@
+package assignment2;
+
+public class Question_1_To_3 {
+
+	public Question_1_To_3() {
+		
+	}
+	public static void main(String[] args) {		
+	}
+	
+/*	1. Write a java function to calculate the salary of an employee based on the following rules.
+
+	i. function takes input of number of hours an employee worked and returns the salary.
+
+	ii.  The first 36 hours worked are paid at a rate of 15.0, then the next 5 hours are paid at a rate of 15 * 1.5. 
+	Hours after that up to a max of 48 are paid at a rate of 15 * 2.
+			
+	
+	Assume  the total hours is always greater than 0 and less than or equal to 48
+*/
+	public  double employeeSalary(double hours) {
+		double payment;
+		final double regularRate =15.0;
+		final double halfRate =1.5;
+		final double overtimeRate =2.0;
+		
+		final double overtimeHour =5.0;
+		final double regularHour =36.0;
+		//final double maxHour = 48.0;
+		final double maxHalfHour = regularHour + overtimeHour;
+		
+		if(hours <= 0)
+			throw new IllegalArgumentException("Min Working hours is 1 hour");
+		if(hours > 48)
+			throw new IllegalArgumentException("Max working hours is 48 hours");
+		if(hours <= regularHour)
+		{
+			payment = hours * regularRate;
+		}
+		else if(hours <= maxHalfHour){
+			payment = regularHour * regularRate + (hours - regularHour)* regularRate * halfRate;			
+		}
+		else {
+			payment = regularHour * regularRate +(maxHalfHour - regularHour)* regularRate * halfRate  + (hours - maxHalfHour)* regularRate * overtimeRate;			
+		}					
+		return payment;
+	}
+
+	
+/*	2. Write a java function that adds all the digits of an integer until it is single digit.
+
+	i. function takes an integer as input and returns its sum of digits.
+
+	ii. for example input = 37, sum = 3+7 = 10, sum = 1+0 = 1. result = 1.
+*/
+	public int addDigits(int input){
+		int sum =0;
+		String stringRep = Integer.toString(input);
+		int length = stringRep.length();
+		for(int i=0; i <=length-1;i++)
+		{
+			sum += Character.getNumericValue(stringRep.charAt(i));
+		}
+		return sum;
+	}
+	
+	
+/*	3. Write a java function to print all perfect number between 1 and n.
+
+	i. Perfect number is a positive integer which is equal to the sum of its proper positive divisors.
+
+
+	ii. For example: 6 is the first perfect number, Proper divisors of 6 are 1, 2, 3. Sum of its proper divisors = 1 + 2 + 3 = 6.
+*/	
+	public void printPerfectNumbers( int n){		
+		int divisorSum =0;
+		for(int divisor =1; divisor <= n/2; divisor++)
+		{
+			if(n % divisor == 0) {
+				divisorSum = divisorSum + divisor;
+			}				
+		}
+		if(divisorSum == n)
+		{
+			System.out.println(n +" is a perfect number");
+		}
+		else {
+			System.out.print(n +" is not a perfect number");
+		}
+	}	
+}
